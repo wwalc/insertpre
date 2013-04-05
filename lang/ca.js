aa0ae354abd94c63380eafa680764da4386d3ee9
Anna Tomanek <a.tomanek@cksource.com>
2013-04-05 13:47:23 +0200

    Catalan, Danish, Canadian French, Portuguese, Ukrainian localization added.

---
+++ lang/ca.js
@@ -0,0 +1,6 @@
+﻿CKEDITOR.plugins.setLang( 'insertpre', 'ca', {
+	title : 'Insereix el fragment de codi',
+	code : 'Codi',
+	edit : 'Editar codi',
+	notEmpty : 'El camp de codi no pot estar buit.'
+});
---
+++ lang/da.js
@@ -0,0 +1,6 @@
+﻿CKEDITOR.plugins.setLang( 'insertpre', 'da', {
+	title : 'Indsæt koden her',
+	code : 'Kode',
+	edit : 'Rediger kode',
+	notEmpty : 'Kodefeltet må ikke være tomt.'
+});
